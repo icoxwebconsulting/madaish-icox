7cb781370c0caa63469043685f8c6511fac30c24
nestor91sanchez <sanchez91nestor@gmail.com>
2017-03-05 15:38:46 -0400

    adding fashionist view

--- www/js/controllers/profile.js
+++
@@ -1,9 +0,0 @@
-app.controller('ProfileController', function ($scope, GLOBAL, PostService) {
-
-    $scope.init = function(){
-        $scope.user = '';
-    };
-
-    $scope.init();
-
-});
---
+++ www/js/directives/parse-links.js
@@ -0,0 +1,48 @@
+app.directive('parseLinks', function($timeout, $state, UtilsService){
+    var parseLink = function(a){
+
+        var href = a.getAttribute('href');
+        var link = UtilsService.parseUrl(href);
+
+        if(!link) return;
+
+        angular.element(a).on('click', function(e){
+
+            e.preventDefault();
+
+            if(link.type == 'external'){
+                UtilsService.openExternalLink(href);
+            }
+            if(link.type == 'post'){
+                $state.go('base.post', {
+                    content: 'post',
+                    userName: link.username,
+                    postName: link.postname
+                });
+            }
+            if(link.type == 'look'){
+                $state.go('base.post', {
+                    content: 'look',
+                    userName: link.username,
+                    postName: link.lookname
+                });
+            }
+            if(link.type == 'unknown'){
+                throw "clicked unknown link type: " + href;
+            }
+
+            return false;
+        });
+    };
+
+    return {
+        restrict: 'A',
+
+        link: function(scope, element, attrs){
+
+            $timeout(function(){
+                angular.forEach(element.find('a'), parseLink);
+            });
+        }
+    };
+});
