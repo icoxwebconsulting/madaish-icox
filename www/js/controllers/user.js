8bba0706e98b9cc64d01fa9439ffa3d8d4c41ab0
leonard518 <lions518@gmail.com>
2017-03-06 12:06:26 -0400

    Added info post

--- www/js/controllers/user.js
+++ www/js/controllers/user.js
@@ -4,8 +4,22 @@ app.controller('UserController', function ($scope, $state, $stateParams, AuthSer
 
         $scope.init = function(){
             $scope.user = [];
+            $scope.posts = [];
             UserService.resource.getFashionist({user: UserService.getUser().Name}).$promise.then(function(data){
                 $scope.user = angular.extend({}, data.Profile, data.extraInfo);
+
+                data.Results.forEach(function(post){
+                    post.Profile = {
+                        UserId: post.UserId,
+                        UserName: post.UserName,
+                        FriendlyUrlUserName: post.FriendlyUrlUserName,
+                        Avatar: post.UserAvatar
+                    };
+                    if(post.ContentType == 1){
+                        post.FriendlyUrlTitle = post.FriendlyUrlLookTitle;
+                    }
+                    post.Type = post.Category.Name;
+                })
             });
             $scope.widget = 'settings';
 
